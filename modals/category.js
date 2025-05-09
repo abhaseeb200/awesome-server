8f977f234b3cdffa718a55e2113134fc82a87c68
abhaseeb200 <haseebabdul747@gmail.com>
2025-05-10 02:15:19 +0500

    Add the createdAt field

--- modals/category.js
+++ modals/category.js
@@ -17,6 +17,10 @@ const CategorySchema = new mongoose.Schema({
     type: String,
     required: [true, "Thumbnail is required"],
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
